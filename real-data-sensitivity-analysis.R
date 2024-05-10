309399aa45d0cd1f857207379a5307543dce15b6
florianstijven <florian.stijven@gmail.com>
2024-05-10 12:15:15 +0200

    Add files for HPC

--- real-data-sensitivity-analysis.R
+++ real-data-sensitivity-analysis.R
@@ -10,7 +10,7 @@ Schizo = na.omit(Schizo)
 
 # Number of unidentifiable copula parameter sets for each data set and
 # additional assumptions.
-n_sim = 5
+n_sim = 20
 # Number of Monte Carlo samples to compute the ICA.
 n_prec = 5e3
 # Number of cores to use in computations.
