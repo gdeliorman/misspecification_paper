2e67f03b52b203cc320bd1c8cbc91bac55f50708
gdeliorman <111044402+gdeliorman@users.noreply.github.com>
2025-10-02 11:13:02 +0200

    Add files via upload

--- PANSS_data.R
+++ PANSS_data.R
@@ -235,27 +235,26 @@ ks.test(new_Schizo, "pnorm")
 ##ICA under normality
 
 ##ICA grid={-1, -0.95,...} 41^4=2825761 6143 pdf matrices
-#ICA<-ICA.ContCont(cT0S0, cT1S1, vT0T0, vT1T1, vS0S0, vS1S1, T0T1=seq(-1, 1, by=.05), 
-      #           T0S1=seq(-1, 1, by=.05), T1S0=seq(-1, 1, by=.05), S0S1=seq(-1, 1, by=.05))
+ICA<-ICA.ContCont(cT0S0, cT1S1, vT0T0, vT1T1, vS0S0, vS1S1, T0T1=seq(-1, 1, by=.05), 
+                T0S1=seq(-1, 1, by=.05), T1S0=seq(-1, 1, by=.05), S0S1=seq(-1, 1, by=.05))
+
+##ICA grid={-1, -0.975,-0.925...} 81^4=43046721 90727 pdf matrices
+ICA<-ICA.ContCont(cT0S0, cT1S1, vT0T0, vT1T1, vS0S0, vS1S1, T0T1=seq(-1, 1, by=.025), 
+                  T0S1=seq(-1, 1, by=.025), T1S0=seq(-1, 1, by=.025), S0S1=seq(-1, 1, by=.025))
+
 
 #ICA grid2={-1, -0.90,...} 21^4=194481 343 pdf matrices 
 ICA_2<-ICA.ContCont(cT0S0, cT1S1, vT0T0, vT1T1, vS0S0, vS1S1, T0T1=seq(-1, 1, by=.1), 
                  T0S1=seq(-1, 1, by=.1), T1S0=seq(-1, 1, by=.1), S0S1=seq(-1, 1, by=.1))
 
-ICA<- ICA_2
-
+#ICA<- ICA_2
 rho_delta<- ICA$ICA
 Ica_normal<- rho_delta^2
-
-mean(Ica_normal)
-max(Ica_normal)
-min(Ica_normal)
 s1<-summary(Ica_normal)
 s1
-quantile(Ica_normal)
 
 
-hist(Ica_normal, xlim = c(0,1), ylim=c(0,200), main=bquote("Histogram of ICA"[N]), xlab=bquote("ICA"[N]))
+hist(Ica_normal, xlim = c(0,1), ylim=c(0,45000), main=bquote("Histogram of ICA"[N]), xlab=bquote("ICA"[N]))
 grid()
 box()
 
@@ -263,14 +262,8 @@ box()
 new_Schizo<- cbind(Schizo$PANSS, Schizo$BPRS)
 sigma_t<- cov(new_Schizo)
 
-##estimate degree of freedom \nu
-fit_log<-fit_mvt(new_Schizo)
-#fit_mvt(new_Schizo, nu = "iterative")
-nu<- fit_log$nu
-
 library(fitHeavyTail)
 nu_2<- nu_POP_estimator(Xc = new_Schizo, nu = 8.12, Sigma = sigma_t)
-#library(MixMatrix)
 
 Ica_t<- ICA_t(
   #df=nrow(Schizo), if it is too big then ICA_t=ICA_N
@@ -281,24 +274,19 @@ Ica_t<- ICA_t(
   T1T1 = vT1T1,
   S0S0 = vS0S0,
   S1S1 = vS1S1,
-  T0T1 = seq(-1, 1, by = 0.1),
-  T0S1 = seq(-1, 1, by = 0.1),
-  T1S0 = seq(-1, 1, by = 0.1),
-  S0S1 = seq(-1, 1, by = 0.1)
+  T0T1 = seq(-1, 1, by = 0.025),
+  T0S1 = seq(-1, 1, by = 0.025),
+  T1S0 = seq(-1, 1, by = 0.025),
+  S0S1 = seq(-1, 1, by = 0.025)
 )
 
 
 Ica_t_dist<- Ica_t$ICA_t
-
-mean(Ica_t_dist)
-max(Ica_t_dist)
-min(Ica_t_dist)
 s2<-summary(Ica_t_dist)
 s2
-quantile(Ica_t_dist)
-sd(Ica_t_dist)
 
-hist(Ica_t_dist, xlim = c(0,1), ylim=c(0,200), main=bquote("Histogram of ICA"[t]), xlab=bquote("ICA"[t]))
+
+hist(Ica_t_dist, xlim = c(0,1), ylim=c(0,45000), main=bquote("Histogram of ICA"[t]), xlab=bquote("ICA"[t]))
 grid()
 box()
 
@@ -327,6 +315,7 @@ AA<- Sur$AA
 mean(Sur$AA.Boot.Samples)
 
 ##ICA log normal: 
+
 mu<- cbind(mT0, mT1, mS0, mS1)
 mu_2<- abs(mu)
 Sigma<- matrix(c(vT0T0, NA, vT0S0, NA,
@@ -352,10 +341,16 @@ cov_convert_sigma_1<- matrix( c( var11, var12, var13, var14,
                                  var13, var23, var33, var34,
                                  var14, var24, var34, var44), nrow = 4, ncol = 4)
 
-T0T1<- seq(-1, 1, by=.05)
-T0S1<- seq(-1, 1, by=.05)
-T1S0<- seq(-1, 1, by=.05)
-S0S1<- seq(-1, 1, by=.05)
+T0T1<- seq(-1, 1, by=.01)
+T0S1<- seq(-1, 1, by=.01)
+T1S0<- seq(-1, 1, by=.01)
+S0S1<- seq(-1, 1, by=.01)
+
+T0T1<- seq(-1, 1, by=.025)
+T0S1<- seq(-1, 1, by=.025)
+T1S0<- seq(-1, 1, by=.025)
+S0S1<- seq(-1, 1, by=.025)
+
 T0T0<- cov_convert_sigma_1[1,1]
 T1T1<- cov_convert_sigma_1[2,2]
 S0S0<- cov_convert_sigma_1[3,3]
@@ -402,12 +397,18 @@ a<-i
 
 diff<- ICA_lognormal_2- ICA_normal_2
 outputs<- cbind(a, rho, ICA_normal_2, ICA_lognormal_2, diff)
-write.table(outputs, file="log_normal_output_4aug.txt",  sep = "\t", row.names = FALSE, col.names = FALSE, append=TRUE)
+#write.table(outputs, file="log_normal_output_4aug.txt",  sep = "\t", row.names = FALSE, col.names = FALSE, append=TRUE)
+write.table(outputs, file="log_normal_output_newgrid_29sep.txt",  sep = "\t", row.names = FALSE, col.names = FALSE, append=TRUE)
 
 
 }
 
 }
+
+data_lognormal_newgrid<- read.table(file="log_normal_output_newgrid_29sep.txt")
+ica_log_normal_newgrid<- as.numeric(unlist(data_lognormal_newgrid[4]))
+plot(density(ica_log_normal_newgrid))
+summary(ica_log_normal_newgrid)
 
 data_lognormal<- read.table(file="log_normal_output_4aug.txt")
 data_lognormal<- read.table(file="/Users/gokcedeliorman/log_normal_output_4aug.txt")
@@ -415,17 +416,10 @@ ica_log_normal<- as.numeric(unlist(data_lognormal[4]))
 hist(ica_log_normal, xlim = c(0,1), ylim=c(0,1200), main=bquote("Histogram of ICA"[L]), xlab=bquote("ICA"[L]))
 grid()
 box()
-summary(ica_log_normal)
-mean(ica_log_normal)
-min(ica_log_normal)
-max(ica_log_normal)
-
-quantile(ica_log_normal)
-sd(ica_log_normal)
 s3<-summary(ica_log_normal)
 s3
 
-##compare ICA_N and ICA_t
+##compare all results
 boxplot(Ica_normal ,Ica_t_dist, ica_log_normal, AA_model,
         names = c(expression(ICA[N]), expression(ICA[t]), expression(ICA[L]), expression(AA (gamma)) ),  ylim=c(0,1)
         #, border = "brown", col="orange")
@@ -444,87 +438,37 @@ boxplot(Ica_normal ,Ica_t_dist, ica_log_normal, AA_model, PTE,
 summary_table <- bind_rows(s1, s2, s3)
 print(summary_table)
 
-###all results histograms and density plots
-hist(Ica_t_dist, xlim = c(0,1), ylim=c(0,200), main=bquote("Histogram of ICA"[t]), xlab=bquote("ICA"[t]))
-grid()
-box()
-
-hist(Ica_normal, xlim = c(0,1), ylim=c(0,200), main=bquote("Histogram of ICA"[N]), xlab=bquote("ICA"[N]))
-grid()
-box()
-
-hist(ica_log_normal, xlim = c(0,1), ylim=c(0,1200), main=bquote("Histogram of ICA"[L]), xlab=bquote("ICA"[L]))
-grid()
-box()
-
-
-hist(Ica_normal, breaks = 15, freq = FALSE, 
-     xlim = c(0,1), ylim = c(0, 35),     # y ekseni daha uzun
-     col = rgb(0,0.5,1,0.4),   
-     main = "Histogram of ICA", 
-     xlab = "ICA", ylab = "")
-
-hist(Ica_t_dist, breaks = 15, freq = FALSE, 
-     col = rgb(1,0.5,0,0.4), add = TRUE)
-
-hist(ica_log_normal, breaks = 15, freq = FALSE, 
-     col = rgb(0,0.6,0,0.3), add = TRUE)
-
-legend("topleft", 
-       legend = c(expression(ICA[N]), expression(ICA[t]), expression(ICA[L])), 
-       fill = c(rgb(0,0.5,1,0.4), rgb(1,0.5,0,0.4), rgb(0,0.6,0,0.3)), 
-       bty = "n")
-
-grid()
-box()
-
-
-par(mfrow = c(1,1))
-df <- data.frame(
-  value = c(Ica_normal, Ica_t_dist, ica_log_normal),
-  group = factor(rep(c("ICA_N", "ICA_t", "ICA_L"), 
-                     times = c(length(Ica_normal), length(Ica_t_dist), length(ica_log_normal))))
-)
-
-# Facet histogram
-ggplot(df, aes(x = value, fill = group)) +
-  geom_histogram(bins = 15, alpha = 0.7, color = "black") +
-  facet_wrap(~ group, nrow = 1, scales = "fixed") +
-  xlim(0,1) +
-  labs(title = "Histograms of ICA", x = "ICA", y = "") +
-  theme_minimal()
-
-
-plot(density(Ica_normal),
-     xlim = c(0,1),     # density için de sınırlama yapabilirsin
-     ylim = c(0, 50), 
-     main = "",
-     xlab = "ICA", 
-     col = "blue", lwd = 2, lty = 1)
-
-lines(density(Ica_t_dist), col = "red", lwd = 2, lty = 2)   # farklı çizgi tipi
-lines(density(ica_log_normal), col = "darkgreen", lwd = 2, lty = 1)
-
-legend("topleft", 
-       legend = c(expression(ICA[N]), expression(ICA[t]), expression(ICA[L])), 
-       col = c("blue", "red", "darkgreen"), 
-       lty = c(1,2,1), lwd = 2, bty = "n")
-grid()
-box()
 
 ###all results saving
+##ICA normal
 save(Ica_normal, file = "Ica_normal_schizo.rda")
 Ica_normal_schizo<-load("Ica_normal_schizo.rda")
 Ica_normal_schizo<-get(Ica_normal_schizo)
 
+##ICA normal with wider grid
+save(Ica_normal,file = "Ica_normal_schizo_newgrid.rda")
+Ica_normal_schizo_newgrid<-load("Ica_normal_schizo_newgrid.rda")
+Ica_normal_schizo_newgrid<-get(Ica_normal_schizo_newgrid)
+
+
+##ICA_t 
 save(Ica_t_dist, file = "Ica_t_schizo.rda")
 Ica_t_schizo<-load("Ica_t_schizo.rda")
 Ica_t_schizo<-get(Ica_t_schizo)
 
+##ICA_t with wider grid
+save(Ica_t_dist, file = "Ica_t_schizo_newgrid.rda")
+Ica_t_schizo_newgrid<-load("Ica_t_schizo_newgrid.rda")
+Ica_t_schizo_newgrid<-get(Ica_t_schizo_newgrid)
+
+##ICA log-normal 
 save(ica_log_normal, file = "Ica_lognormal_schizo.rda")
 Ica_lognormal_schizo<-load("Ica_lognormal_schizo.rda")
 Ica_lognormal_schizo<-get(Ica_lognormal_schizo)
 
-
+##ICA log-normal with wider grid
+save(ica_log_normal_newgrid, file = "Ica_lognormal_schizo_newgrid.rda")
+Ica_lognormal_schizo_newgrid<-load("Ica_lognormal_schizo_newgrid.rda")
+Ica_lognormal_schizo_newgrid<-get(Ica_lognormal_schizo_newgrid)
 
 
